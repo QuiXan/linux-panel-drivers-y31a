414be4ea3c26f5c3ad4dc0550ee85e5d43f89065
QuiXan <126559216+QuiXan@users.noreply.github.com>
2026-04-19 21:30:28 +0800

    Create vivo-pd1505f.sh

---
+++ config/vivo-pd1505f.sh
@@ -0,0 +1,7 @@
+# DTB: From Android boot partition extraction
+OPTIONS=(-r vdd3 -r vci --no-backlight --ignore-wait 1)
+PANELS=(
+  [pd1505l_otm9605a_540p]="vivo,otm9605a"
+  [pd1505l_otm9605a_540p-baoming]="vivo,otm9605a-baoming"
+  [pd1505l_otm9605a_540p-boenew]="vivo,otm9605a-boenew"
+)

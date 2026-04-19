b13e40327dd67a06ceea6b43d8e6f99a2516d830
QuiXan <126559216+QuiXan@users.noreply.github.com>
2026-04-19 21:42:00 +0800

    Update vivo-pd1505f.sh

--- config/vivo-pd1505f.sh
+++ config/vivo-pd1505f.sh
@@ -1,7 +1,7 @@
 # DTB: From Android boot partition extraction
 OPTIONS=(-r vdd3 -r vci --no-backlight --ignore-wait 1)
 PANELS=(
-  [pd1505l_otm9605a_540p]="vivo,otm9605a"
-  [pd1505l_otm9605a_540p-baoming]="vivo,otm9605a-baoming"
-  [pd1505l_otm9605a_540p-boenew]="vivo,otm9605a-boenew"
+  [pd1505l_otm9605a_540p_video]="vivo,otm9605a"
+  [pd1505l_otm9605a_540p_video_baoming]="vivo,otm9605a-baoming"
+  [pd1505l_otm9605a_540p_video_boenew]="vivo,otm9605a-boenew"
 )
